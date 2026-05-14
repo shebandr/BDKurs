be7927bda18eaa7ba46db9185834fb0f64a1ee99
andreyeyeye <andreyeyeye@DESKTOP-TGB9CN7>
2026-05-14 17:26:33 +0300

    я не помню, но что-то важное

--- BDKurs/Pages/Query.cshtml.cs
+++ BDKurs/Pages/Query.cshtml.cs
@@ -29,11 +29,18 @@ namespace BDKurs.Pages
             int query = Int32.Parse(QueryButton);
             string Day = "";
 
-            string firstQuery = "SELECT t.TrainingName, t.Day, tr.FirstName AS TrainerFirstName, tr.LastName AS TrainerLastName FROM Members m JOIN MemberTrainings mt ON m.MemberID = mt.MemberID JOIN Trainings t ON mt.TrainingID = t.TrainingID JOIN Trainers tr ON t.TrainerID = tr.TrainerID WHERE m.FirstName = 'Екатерина' AND m.LastName = 'Васильева';";
+            string firstQuery = "SELECT t.TrainingName, t.Day, tr.FirstName AS TrainerFirstName, tr.LastName AS TrainerLastName " +
+                                "FROM Members m JOIN MemberTrainings mt ON m.MemberID = mt.MemberID " +
+                                "JOIN Trainings t ON mt.TrainingID = t.TrainingID " +
+                                "JOIN Trainers tr ON t.TrainerID = tr.TrainerID WHERE m.FirstName = 'Екатерина' AND m.LastName = 'Васильева';";
 
-            string secondQuery = "SELECT m.FirstName, m.LastName FROM Members m JOIN MemberTrainings mt ON m.MemberID = mt.MemberID GROUP BY m.MemberID HAVING COUNT(mt.TrainingID) > 1;";
+            string secondQuery = "SELECT m.FirstName, m.LastName FROM Members m " +
+                "JOIN MemberTrainings mt ON m.MemberID = mt.MemberID " +
+                "GROUP BY m.MemberID HAVING COUNT(mt.TrainingID) > 1;";
             
-            string thirdQuery = "SELECT DISTINCT m.FirstName, m.LastName FROM Members m JOIN MemberTrainings mt ON m.MemberID = mt.MemberID JOIN Trainings t ON mt.TrainingID = t.TrainingID WHERE t.Day = '";
+            string thirdQuery = "SELECT DISTINCT m.FirstName, m.LastName FROM Members m " +
+                "JOIN MemberTrainings mt ON m.MemberID = mt.MemberID " +
+                "JOIN Trainings t ON mt.TrainingID = t.TrainingID WHERE t.Day = '";
 
 
 
